09ca28fc2385cc9e99c5be7faf096f0ba249067a
Maik Ploigt <maik.ploigt@freiheit.com>
2025-03-21 14:00:22 +0100

    Fix some layout issues
    FTN-N7XA3M - Get in touch mobile – adjust spacing
    FTN-GFBZ5C - Spacing + aligned texts and pictures
    FTN-SPK2HH - Navigation black box
    FTN-XHN5UQ - Header Stage mobile – adjust spacing

--- assets/js/_navigation.js
+++ assets/js/_navigation.js
@@ -9,14 +9,14 @@ document.getElementById('navigation_close').addEventListener("click", function (
 let isOnTop = false;
 const onScroll = function () {
     const navigation = document.getElementsByTagName('nav')[0];
-    const firstContentItemTop = document.querySelectorAll('main > .fdc-content-layout')[0].getBoundingClientRect().top;
-    if (!isOnTop && (firstContentItemTop < 65)) {
+    const firstContentItemTop = document.querySelectorAll('h1')[0].getBoundingClientRect().top;
+    if (firstContentItemTop && !isOnTop && (firstContentItemTop < 65)) {
         navigation.classList.add('bg-black-nav');
         navigation.classList.add('text-white-nav');
         navigation.classList.remove('md:pt-6');
         navigation.classList.remove('bg-transparent');
         isOnTop = true;
-    } else if (firstContentItemTop > 65) {
+    } else if (firstContentItemTop && firstContentItemTop > 65) {
         navigation.classList.remove('bg-black-nav');
         navigation.classList.remove('text-white-nav');
         navigation.classList.add('md:pt-6');
