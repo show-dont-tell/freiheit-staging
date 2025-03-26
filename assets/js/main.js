3294159cb67cddd76c0b2858e13b4f365bcdc5ce
Maik Ploigt <maik.ploigt@freiheit.com>
2025-03-26 15:17:57 +0100

    Fingerprint js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
-import './_navigation.js';
+const checkbox = document.getElementById('open-menu-checkbox')
+
+document.getElementById('navigation_close').addEventListener("click", function (e) {
+    e.preventDefault()
+    checkbox.checked = false;
+}, {passive: true});
+
+/* set header background on home page to make it visible after header video */
+let isOnTop = false;
+const onScroll = function () {
+    const navigation = document.getElementsByTagName('nav')[0];
+    const firstContentItemTop = document.querySelectorAll('h1')[0].getBoundingClientRect().top;
+    if (firstContentItemTop && !isOnTop && (firstContentItemTop < 65)) {
+        navigation.classList.add('bg-black-nav');
+        navigation.classList.add('text-white-nav');
+        navigation.classList.remove('md:pt-6');
+        navigation.classList.remove('bg-transparent');
+        isOnTop = true;
+    } else if (firstContentItemTop && firstContentItemTop > 65) {
+        navigation.classList.remove('bg-black-nav');
+        navigation.classList.remove('text-white-nav');
+        navigation.classList.add('md:pt-6');
+        navigation.classList.add('bg-transparent');
+        isOnTop = false;
+    }
+}
+onScroll();
+window.addEventListener('scroll', onScroll, { passive: true })
